d461445d71cc10c11c2af11b815863aef0ff4959
Ross Hadden <rosshadden@gmail.com>
2023-01-23 17:13:14 -0500

    feat: basic event loop
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate device_query;
+
+use device_query::{DeviceEvents, DeviceState, Keycode};
+
+struct Bocks {
+	state: DeviceState,
+}
+
+impl Bocks {
+	fn new() -> Self {
+		let state = DeviceState::new();
+
+		Self { state }
+	}
+
+	fn start(&self) {
+		let _guard = self.state.on_key_down(|key| {
+			println!("down: {:#?}", key);
+			// self.process(key);
+		});
+
+		let _guard = self.state.on_key_up(|key| {
+			println!("up: {:#?}", key);
+		});
+
+		loop {}
+	}
+
+	fn process(&self, key: &Keycode) {
+	}
+}
+
 fn main() {
-	println!("Hello, world!");
+	let bocks = Bocks::new();
+	bocks.start();
 }
